43d1d75d8d125071e89137db88e6b93c15318cd8
Hugo Barreto Ferreira <hugobarreto@ibm.com>
2022-04-04 20:57:27 -0300

    validator

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,45 +1,61 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api')
 
-// const User = mongoose.model('User', {
-//     name: {
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Esse e-mail é inválido')
+            }
+        }
+    },
+    age: {
+        type: Number,
+        validate(value) {
+            if ( value < 0) {
+                throw new Error('A idade deve ser um número positivo')
+            }
+        } 
+    }
+})
+
+const me = new User({
+    name: 'Hugo',
+    email: 'hugo@teste.com'
+})
+
+me.save().then(() => {
+    console.log(me)
+}).catch((error) => {
+    console.log('Erro', error)
+})
+
+
+// const Tasks = mongoose.model('Tasks', {
+//     description: {
 //         type: String
 //     },
-//     age: {
-//         type: Number
+//     completed : {
+//         type: Boolean
 //     }
+
 // })
 
-// const me = new User({
-//     name: 'Hugo',
-//     age: 21
+// const task = new Tasks({
+//     description: 'Levar o lixo',
+//     completed: false
 // })
 
-// me.save().then(() => {
-//     console.log(me)
+// task.save().then(() => {
+//     console.log(task)
 // }).catch((error) => {
-//     console.log('Erro', error)
+//     console.log('Ops! Algo de errado!', error)
 // })
-
-
-const Tasks = mongoose.model('Tasks', {
-    description: {
-        type: String
-    },
-    completed : {
-        type: Boolean
-    }
-
-})
-
-const task = new Tasks({
-    description: 'Levar o lixo',
-    completed: false
-})
-
-task.save().then(() => {
-    console.log(task)
-}).catch((error) => {
-    console.log('Ops! Algo de errado!', error)
-})
