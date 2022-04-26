dcd2c75454c4cb2841103d18403c6a8ff2292306
Hugo Barreto Ferreira <hugobarreto@ibm.com>
2022-04-26 01:58:03 -0300

    playground

--- playground/8-promises.js
+++ playground/8-promises.js
@@ -1,13 +1,42 @@
-const doWorkPromise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        // resolve([7, 4, 1])
-        reject('As coisas deram errado')
-        resolve([2, 3, 2])
-    }, 2000)
+// const doWorkPromise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         // resolve([7, 4, 1])
+//         reject('As coisas deram errado')
+//         resolve([2, 3, 2])
+//     }, 2000)
+// })
+
+// doWorkPromise.then((result) => {
+//     console.log('Sucesso!', result)
+// }).catch((error) => {
+//     console.log('Erro!', error)
+// })
+
+const add = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(a + b)
+        }, 2000)
+    })
+}
+
+add(1, 2).then((sum) => {
+    console.log(sum)
+
+    add(sum, 5).then((sum2) => {
+        console.log(sum2)
+    }).catch((e) => {
+        console.log(e)
+    })
+}).catch((e) => {   
+    console.log(e)
 })
 
-doWorkPromise.then((result) => {
-    console.log('Sucesso!', result)
-}).catch((error) => {
-    console.log('Erro!', error)
-})
+// add(1, 1).then((sum) => {
+//     console.log(sum)
+//     return add(sum, 4)
+// }).then((sum2) => {
+//     console.log(sum2)
+// }).catch((e) => {
+//     console.log(e)
+// })
